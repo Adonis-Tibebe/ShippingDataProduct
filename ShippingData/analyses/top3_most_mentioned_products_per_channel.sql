d8afd068f099053f52aef77a52c31b81eaecfa9c
Adoniyas Tibebe <adatibebe12@gmail.com>
2025-07-13 23:34:27 +0300

    added the analyses directory hosing a top3 product mentioned product selection sql script for run in pgadmin

---
+++ ShippingData/analyses/top3_most_mentioned_products_per_channel.sql
@@ -0,0 +1,26 @@
+
+
+with mention_counts as (
+    select
+        p.product_name,
+        c.channel_name,
+        count(*) as mention_count
+    from "shipping_data"."mart"."dim_product_mention" p
+    join "shipping_data"."mart"."fct_messages" f
+      on p.message_metadata_id = f.message_metadata_id
+    join "shipping_data"."mart"."dim_channel" c
+      on f.channel_id = c.channel_id
+    group by p.product_name, c.channel_name
+),
+ranked as (
+    select *,
+           row_number() over (
+               partition by channel_name
+               order by mention_count desc
+           ) as rank
+    from mention_counts
+)
+select *
+from ranked
+where rank <= 3
+order by channel_name, rank
